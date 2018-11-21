967d5390f24aeeb434c51685de32a10eb5b2711a
yanting0928 <1303697177@qq.com>
2018-11-21 20:06:27 +0800

    add capping, only protein

--- tst-pdb-select.py
+++ tst-pdb-select.py
@@ -1,4 +1,5 @@
 import os
+from libtbx import easy_run
 import iotbx.pdb
 from iotbx.pdb import remark_2_interpretation
 import mmtbx.model
@@ -27,7 +28,7 @@ def not_only_protein(pdb_hierarchy):
     for chain in model.chains():
       for rg in chain.residue_groups():
         for ag in rg.atom_groups():
-          if get_class(ag.resname) == "common_rna_dna":
+          if  not (get_class(ag.resname) == "common_amino_acid" or  get_class(ag.resname) == "modified_amino_acid"):
             return True 
   return False
 
@@ -46,7 +47,7 @@ def box_pdb(pdb_inp,filename):
     buffer_layer=10)
   model.set_sites_cart(box.sites_cart)
   cs  = box.crystal_symmetry()
-  box_file=open(filename[:4]+"_box.pdb",'w')
+  box_file=open(filename+"_box.pdb",'w')
   box_file.write(model.model_as_pdb())
   box_file.close()
   return cs
@@ -58,9 +59,15 @@ def remove_rna_dna(pdb_hierarchy):
     for chain in model.chains():
       for rg in chain.residue_groups():
         for ag in rg.atom_groups():
-          if get_class(ag.resname) == "common_rna_dna":
+          if  not (get_class(ag.resname) == "common_amino_acid" or  get_class(ag.resname) == "modified_amino_acid"):
             not_protein_resname.append(ag.resname.strip())
-  return not_protein_resname
+  not_protein_resname_h=list(set(not_protein_resname))
+  selection=" and ".join("not resname %s"%i for i in not_protein_resname_h)
+  asc = pdb_hierarchy.atom_selection_cache()
+  sel = asc.selection(selection)
+  pdb_hierarchy = pdb_hierarchy.select(sel)
+
+  return pdb_hierarchy 
 
 def clusters(pdb_hierarchy):
   yoink_utils.write_yoink_infiles("cluster.xml",
@@ -80,30 +87,29 @@ def clusters(pdb_hierarchy):
  
  
 def run(file_name):
+  filename=os.path.basename(file_name)[:4]
   pdb_inp = iotbx.pdb.input(file_name = file_name)
-  pdb_hierarchy = pdb_inp.construct_hierarchy()
   resolution = get_resolution(pdb_inp = pdb_inp)
   data_type = pdb_inp.get_experiment_type()
-  filename=os.path.basename(file_name)
-  asc = pdb_hierarchy.atom_selection_cache()
   if resolution > 0.9: 
     print resolution
     if data_type=="X-RAY DIFFRACTION" or  data_type=="NEUTRON DIFFRACTION":
       print data_type
+      easy_run.go("phenix.reduce %s > %s" %(file_name,filename+"_reduce.pdb"))
+      file_name = filename+"_reduce"
+      pdb_inp = iotbx.pdb.input(file_name = file_name+".pdb")
+      pdb_hierarchy = pdb_inp.construct_hierarchy()
       if not_only_protein(pdb_hierarchy=pdb_hierarchy): 
         print "PDB file not only protein"
-        not_protein_resname = remove_rna_dna(pdb_hierarchy=pdb_hierarchy)
-        selection=" and ".join("not resname %s"%i for i in not_protein_resname)
-        print selection
-        sel = asc.selection(selection)
-        pdb_hierarchy = pdb_hierarchy.select(sel)
-  #    pdb_hierarchy.write_pdb_file(file_name="new_pdb.pdb")
+        pdb_hierarchy  = remove_rna_dna(pdb_hierarchy=pdb_hierarchy)
       if have_conformers(pdb_hierarchy=pdb_hierarchy):
         pdb_hierarchy.remove_alt_confs(always_keep_one_conformer=True)
-      pdb_hierarchy.write_pdb_file(file_name = filename[:3]+"_new.pdb")
-      pdb_inp = iotbx.pdb.input(file_name = filename[:3]+"_new.pdb")
-      cs = box_pdb(pdb_inp=pdb_inp,filename=filename)
-      fq = fragments(pdb_hierarchy=pdb_hierarchy,
+      file_name = file_name+"_update"
+      pdb_hierarchy.write_pdb_file(file_name = file_name+".pdb")
+      pdb_inp = iotbx.pdb.input(file_name = file_name+".pdb")
+      cs = box_pdb(pdb_inp=pdb_inp,filename=file_name)
+      fq = fragments(pdb_hierarchy= iotbx.pdb.input(file_name = file_name+"_box.pdb").construct_hierarchy(),
+                     working_folder = filename,
                      maxnum_residues_in_cluster=5,
                      debug=True,
                      crystal_symmetry=cs)
@@ -113,7 +119,8 @@ def run(file_name):
         get_qm_file_name_and_pdb_hierarchy(
                           fragment_extracts=fq_ext,
                           index=i)
-  #clusters(pdb_hierarchy=pdb_hierarchy_new)
-  #pdb_hierarchy_new.write_pdb_file(file_name="new_pdb.pdb")
+      libtbx.easy_run.fully_buffered("mv %s_* %s/"%(filename,filename))
+      libtbx.easy_run.fully_buffered("rm *.pdb")
+      
 if __name__ == '__main__':
   result = run(file_name = "/home/yanting/QR/ANI/qr-work/1bdw.pdb")
