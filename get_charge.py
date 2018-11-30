734ddfaece99805d6910db503c73ef8a6ded9e38
yanting0928 <1303697177@qq.com>
2018-11-30 17:34:19 +0800

    remove files

--- get_charge.py
+++
@@ -1,7 +0,0 @@
-from qrefine.charges import charges_class
-import os
-
-fname = "2_cluster_capping.pdb"
-charge = charges_class(pdb_filename=fname).get_total_charge()
-os.rename(fname, str(charge)+'_'+fname)
-print charge
