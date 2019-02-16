7d22f2fe5fb0fadefd07d98b382f64b78faa29f3
yanting0928 <1303697177@qq.com>
2019-02-16 11:12:27 +0800

    lbfgs minimize option

--- deviation_geo.py
+++ deviation_geo.py
@@ -8,6 +8,7 @@ from cStringIO import StringIO
 import mmtbx
 import os
 import sys
+import scitbx
 from scitbx.math import superpose
 from libtbx.utils import null_out
 from iotbx.file_reader import any_file
@@ -18,6 +19,9 @@ from libtbx.utils import null_out
 from cctbx import maptbx
 import mmtbx.hydrogens
 from mmtbx.utils import run_reduce_with_timeout
+from libtbx import adopt_init_args
+from cctbx import xray
+from phenix.refinement import weight_xray_chem
 
 #def ccp4_map(cg, file_name, map_data):
 #  from iotbx import mrcfile
@@ -79,7 +83,7 @@ from mmtbx.utils import run_reduce_with_timeout
 
 def move_residue_atoms(map_data, atom, crystal_symmetry):
   sites_cart = atom.xyz
-  xyz_best = maptbx.fit_point_3d_grid_search(site_cart=sites_cart,
+  xyz_best = maptbx.fit_point_3d_grid_search(site_cart=sites_cart, 
     map_data=map_data, unit_cell=crystal_symmetry.unit_cell(), amplitude=0.1,
     increment=0.001)
   diff = list(flex.vec3_double([sites_cart])-flex.vec3_double([xyz_best]))[0]
@@ -176,7 +180,63 @@ def get_model(pdb_file_name):
   sel = model.selection(string = "element H and not protein")
   return model.select(~sel)
 
-def run(pdb_file_name, data_file_name, nproc):
+class lbfgs(object):
+  def __init__(self, fmodels,
+                     number_of_iterations = 25):
+    adopt_init_args(self, locals())
+    self.f=None
+    self.xray_structure = self.fmodels.fmodel_xray().xray_structure
+    self.correct_special_position_tolerance = 1.0
+    self.x = flex.double(self.xray_structure.n_parameters(), 0)
+    self._scatterers_start = self.xray_structure.scatterers()
+    self.weights = weight_xray_chem.weights(wx       = 1,
+                                            wx_scale = 1,
+                                            angle_x  = None,
+                                            wn       = 1,
+                                            wn_scale = 1,
+                                            angle_n  = None,
+                                            w        = 0,
+                                            wxn      = 1)
+    lbfgs_termination_params = scitbx.lbfgs.termination_parameters(
+      max_iterations = number_of_iterations)
+    self.minimizer = scitbx.lbfgs.run(
+      target_evaluator          = self,
+      termination_params        = lbfgs_termination_params,
+      use_fortran               = False,
+      exception_handling_params = scitbx.lbfgs.exception_handling_parameters(
+                         ignore_line_search_failed_step_at_lower_bound = True))
+    self.apply_shifts()
+    del self._scatterers_start
+    self.compute_target(compute_gradients = False,u_iso_refinable_params = None)
+    self.fmodels.create_target_functors()
+  
+  def apply_shifts(self):
+    apply_shifts_result = xray.ext.minimization_apply_shifts(
+      unit_cell      = self.xray_structure.unit_cell(),
+      scatterers     = self._scatterers_start,
+      shifts         = self.x)
+    scatterers_shifted = apply_shifts_result.shifted_scatterers
+    self.xray_structure.replace_scatterers(scatterers = scatterers_shifted)
+    
+  def compute_target(self, compute_gradients, u_iso_refinable_params):
+    self.stereochemistry_residuals = None
+    self.fmodels.update_xray_structure(
+      xray_structure = self.xray_structure,
+      update_f_calc  = True)
+    fmodels_target_and_gradients = self.fmodels.target_and_gradients(
+      weights                = self.weights,
+      compute_gradients      = compute_gradients,
+      u_iso_refinable_params = u_iso_refinable_params)
+    self.f = fmodels_target_and_gradients.target()
+    self.g = fmodels_target_and_gradients.gradients()
+  
+  def compute_functional_and_gradients(self):
+    u_iso_refinable_params = self.apply_shifts()
+    self.compute_target(compute_gradients     = True,
+                        u_iso_refinable_params = u_iso_refinable_params)
+    return self.f, self.g
+
+def run(pdb_file_name, data_file_name, nproc, method="lbfgs"):
   pdb_code = os.path.basename(pdb_file_name)[:4]
   model = get_model(pdb_file_name = pdb_file_name)
   crystal_symmetry = model.crystal_symmetry()
@@ -193,49 +253,58 @@ def run(pdb_file_name, data_file_name, nproc):
   if(fmodel_ini.r_work()*100.>20):
     print "  skip: too large starting Rwork, inputs must be bad."
     return # SKIP
-  unit_cell = crystal_symmetry.unit_cell()
-  tfofc = get_map(
-    fmodel=fmodel_ini, map_type="2mFo-DFc", scale=True)
   show(model = model, fmodel = fmodel_ini, prefix = "Start:")
-  get_class = iotbx.pdb.common_residue_names_get_class
-  atom_seq=[]
-  for model_ in pdb_hierarchy.models():
-    for chain in model_.chains():
-      for residue_group in chain.residue_groups():
-        for conformer  in residue_group.conformers():
-          for residue in conformer.residues():
-            if(get_class(name=residue.resname) == "common_water"): continue
-            for atom in residue.atoms():
-              if(atom.element.strip().upper()=="H"): continue
-              map_at_center = tfofc.tricubic_interpolation(
-                unit_cell.fractionalize(atom.xyz))
-              if(map_at_center<1.0): continue
-              atom_seq.append(atom.i_seq)
-  args = [(atom_seq[i], pdb_hierarchy, xray_structure, crystal_symmetry,
-    fmodel_ini) for i in range(len(atom_seq))]
-  results = pool_map(
-    func      = map_peak_coordinate,
-    iterable  = args,
-    processes = nproc)
-  site_carts = flex.vec3_double(pdb_hierarchy.atoms().extract_xyz())
-  select = flex.size_t([i for i in atom_seq])
-  site_cart_shifted = flex.vec3_double(results)
-  site_cart_new = site_carts.set_selected(select,site_cart_shifted)
-  pdb_hierarchy.atoms().set_xyz(site_cart_new)
-  pdb_hierarchy.write_pdb_file(file_name="%s_updated.pdb"%pdb_code,
-    crystal_symmetry = crystal_symmetry)
+  if method == "lbfgs":
+    fmodel_ini.xray_structure.scatterers().flags_set_grad_site(
+      iselection = xray_structure.all_selection().iselection())
+    fmodels = mmtbx.fmodels(fmodel_xray = fmodel_ini)
+    minimized = lbfgs(fmodels = fmodels)
+    pdb_hierarchy.adopt_xray_structure(minimized.xray_structure)
+    
+  elif method == "map-match":
+    unit_cell = crystal_symmetry.unit_cell()
+    tfofc = get_map(
+      fmodel=fmodel_ini,  map_type="2mFo-DFc", scale=True)
+    get_class = iotbx.pdb.common_residue_names_get_class
+    atom_seq=[]
+    for model_ in pdb_hierarchy.models():
+      for chain in model_.chains():
+        for residue_group in chain.residue_groups():
+          for conformer  in residue_group.conformers():
+            for residue in conformer.residues():
+              if(get_class(name=residue.resname) == "common_water"): continue
+              for atom in residue.atoms():
+                if(atom.element.strip().upper()=="H"): continue
+                map_at_center = tfofc.tricubic_interpolation(
+                  unit_cell.fractionalize(atom.xyz))
+                if(map_at_center<1.0): continue
+                atom_seq.append(atom.i_seq)
+    args = [(atom_seq[i], pdb_hierarchy, xray_structure, crystal_symmetry,
+      fmodel_ini) for i in range(len(atom_seq))]
+    results = pool_map(
+      func      = map_peak_coordinate,
+      iterable  = args,
+      processes = nproc)
+    site_carts = flex.vec3_double(pdb_hierarchy.atoms().extract_xyz())
+    select = flex.size_t([i for i in atom_seq])
+    site_cart_shifted = flex.vec3_double(results)
+    site_cart_new = site_carts.set_selected(select,site_cart_shifted)
+    pdb_hierarchy.atoms().set_xyz(site_cart_new)
+
   xray_structure = pdb_hierarchy.extract_xray_structure(
     crystal_symmetry = crystal_symmetry)
   model.set_sites_cart(sites_cart = xray_structure.sites_cart())
   model.idealize_h_riding()
   fmodel_ini.update_xray_structure(xray_structure = model.get_xray_structure(),
     update_f_calc=True, update_f_mask=True)
-  fmodel_ini.update_all_scales()
+  fmodel_ini.update_all_scales() 
+  pdb_hierarchy.write_pdb_file(file_name="%s_updated.pdb"%pdb_code,
+      crystal_symmetry = crystal_symmetry)
   show(model = model, fmodel = fmodel_ini, prefix = "Final:")
 
 if __name__ == '__main__':
 #  exercise()
-  if 1:
+  if 0:
     path = "/net/anaconda/raid1/afonine/work/high_res_survey/high_res_pdb_mtz/"
     pdbs  = flex.std_string()
     mtzs  = flex.std_string()
@@ -272,5 +341,5 @@ if __name__ == '__main__':
       print code, "-"*75
       run(pdb_file_name=pdb_file, data_file_name=mtz_file, nproc=50)
   else:
-    run(pdb_file_name="3nir_refine_001.pdb",
-        data_file_name="3nir_refine_data.mtz", nproc=90)
+    run(pdb_file_name="1akg.pdb",
+        data_file_name="1akg.mtz", nproc=8, method="map-match")
