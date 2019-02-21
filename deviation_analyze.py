397d41404419f3b9099302a0c86d3c8347082aa5
yanting0928 <1303697177@qq.com>
2019-02-21 10:45:50 +0800

    flip symmetric amino acids

--- deviation_analyze.py
+++ deviation_analyze.py
@@ -10,10 +10,13 @@ from cctbx import geometry_restraints
 
 def get_model(pdb_file_name,selection):
   pdb_inp = iotbx.pdb.input(file_name = pdb_file_name)
+  params =  mmtbx.model.manager.get_default_pdb_interpretation_params()
+  params.pdb_interpretation.flip_symmetric_amino_acids = True
   model = mmtbx.model.manager(
-    model_input       = pdb_inp,
-    stop_for_unknowns = False,
-    log               = null_out())
+    model_input               = pdb_inp,
+    pdb_interpretation_params = params,
+    stop_for_unknowns         = False,
+    log                       = null_out())
   sel = model.selection(string = selection)
   return model.select(sel) 
 
@@ -22,27 +25,28 @@ def run(pdb_file_name):
   resname = "ARG"
   atom_name = ["CZ", "NE", "NH1", "NH2", "CD"]
   selection = "resname " + resname + " and name " + " or name ".join(i for i in atom_name)
+  print selection
   model = get_model(pdb_file_name = pdb_file_name, selection = selection)
-#  print model.model_as_pdb()
   pdb_hierarchy = model.get_hierarchy()
+  pdb_hierarchy.write_pdb_file(file_name = "test.pdb")
   grm = model.get_restraints_manager().geometry
-  ph = model.get_hierarchy()
-  sites_cart = ph.atoms().extract_xyz()
+  sites_cart = pdb_hierarchy.atoms().extract_xyz()
+  atoms = pdb_hierarchy.atoms()
   for proxy in grm.angle_proxies:
     angle_proxy = proxy.i_seqs
-    atom0 = ph.atoms()[angle_proxy[0]]
-    atom1 = ph.atoms()[angle_proxy[1]]
-    atom2 = ph.atoms()[angle_proxy[2]]
+    atom0 = atoms[angle_proxy[0]]
+    atom1 = atoms[angle_proxy[1]]
+    atom2 = atoms[angle_proxy[2]]
     angle = geometry_restraints.angle(
       sites_cart = sites_cart,
       proxy      = proxy)
     print "%s-%s-%s angle:"%(atom0.name,atom1.name,atom2.name),angle.angle_model
   for proxy in grm.dihedral_proxies:
     dihedral_proxy = proxy.i_seqs
-    atom0 = ph.atoms()[dihedral_proxy[0]]
-    atom1 = ph.atoms()[dihedral_proxy[1]]
-    atom2 = ph.atoms()[dihedral_proxy[2]]
-    atom3 = ph.atoms()[dihedral_proxy[3]]
+    atom0 = atoms[dihedral_proxy[0]]
+    atom1 = atoms[dihedral_proxy[1]]
+    atom2 = atoms[dihedral_proxy[2]]
+    atom3 = atoms[dihedral_proxy[3]]
     dihedral = geometry_restraints.dihedral(
       sites_cart = sites_cart,
       proxy = proxy)
